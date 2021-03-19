1d0349d5b30d8c38cfe1069a251b069c04f19d75
C-83 <reiman.c83@gmail.com>
2021-03-18 23:44:45 -0700

    updated server.js with html start page

--- server.js
+++ server.js
@@ -1,47 +1,31 @@
-//modules
+//server module
 const express = require('express');
+var fs = require('fs');
+var fetch = require('node-fetch');
+var url = "https://api.sunrise-sunset.org/json?lat=40.23810&lng=24.20133&date=today";
+
 const app = express();
-//new to mongodb need training wheels
-var mongoose = require('mongoose');
 
-
-//set port
+//port set
 const port = 3000;
-//config files db path defined
-var db = require('./config/db');
-console.log("connecting....",db);
-//connect to local db at alternate port to site
+app.get('/',function(req,res){ 
 
-  //stuff here for CRUD
-
-mongoose.connect(db.url,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify:false
-});
-
-
-//Home Route
-app.get('/', (req,res) => res.send('Here is HOME'));
-
-//define Route
-app.get('/newb', function (req,res){
-  res.send('Newb List: ');
-});
-
-//sampling api Route
-//grab the newb we just created
-var newb = require('./app/models/newb');
-app.get('/api/newbs', function(req,res){
-  //using mongoose to get newb
-  newb.find(function(err,newbs){
-    if(err)
-      res.send(err);
-    res.json(newbs);
+    fs.readFile('startPage.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+   
+    return res.end('<button onclick="athosfetch()">Get Athos</button><br><button onclick="foobar()">foobar</button><br>');
+    
   });
+    
 });
 
 
-//startup app at localhost:3000
-app.listen(port,() => console.log('App is listening on port 3000!'));
+
+//routes from main
+app.get('/foobar',function(req,res){
+res.send('This is foobar page...');
+});
+
+//listening at port
+app.listen(port, () => console.log('It works'));
